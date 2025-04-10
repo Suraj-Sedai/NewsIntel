ddab656a08e3a498870443b0ed4b06f884f5abb3
Suraj Sedai <surajseda05@gmail.com>
2025-04-10 16:32:48 -0400

    database set to defult one more

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'your_db_name',
-        'USER': 'your_db_user',
-        'PASSWORD': 'your_db_password',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
