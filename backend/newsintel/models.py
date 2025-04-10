fb7ed1cb04d575e9e467180e6156a56ca22f47c4
Suraj Sedai <surajseda05@gmail.com>
2025-04-10 16:27:09 -0400

    model setup

--- backend/newsintel/models.py
+++ backend/newsintel/models.py
@@ -8,4 +8,17 @@ class  Article(models.Model):
     sentiment = models.CharField(max_length=20)
     summary = models.TextField()
     topics = models.TextField()
-    published_at = models.DateTimeField()
+    timestamp = models.DateTimeField()
+
+'''b. User Preferences Model
+Associate user preferences with Django’s built-in User model. For example:
+
+Preferred topics (Technology, Politics, Science, etc.).
+
+History of interacted or read articles (possibly using a many-to-many field).'''
+
+class UserPreferences(models.Model):
+    user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+    preferred_topics = models.TextField()
+    read_articles = models.ManyToManyField(Article, blank=True)
+    
